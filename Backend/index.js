966fbde988119d88192ca7fc3425d82da8224639
user-Zarin <xarinkhan45@gmail.com>
2025-07-27 22:17:58 +0530

    irrelevant things removed

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://second-hand-store-9mgh.vercel.app/",
+    origin: "https://second-hand-store-9mgh.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }))
